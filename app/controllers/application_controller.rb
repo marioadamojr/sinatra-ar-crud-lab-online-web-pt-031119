74b87ea93075429fe925f5dca3ac51297cb26e64
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-28 00:10:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/articles/:id' do
-    Article.destory(params[:id])
+    Article.destroy(params[:id])
     redirect to '/articles'
   end
 end
